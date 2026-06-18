f71ae71fd3b5a0501348dd5005c1e4c1173b5149
Siddharth T <siddharthtamilselvan1@gmail.com>
2026-06-18 22:07:25 +0530

    test: fix broken widget test reference

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:muvi/main.dart';
+import 'package:muvi/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MuviApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
